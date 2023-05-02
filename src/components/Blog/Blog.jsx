fe493e9724b6d3666d904300f2a81c2406df79a8
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-02 20:45:54 +0600

    added blog section"

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = () => {
+    return (
+        <div>
+            <h2>blog section</h2>
+        </div>
+    );
+};
+
+export default Blog;
