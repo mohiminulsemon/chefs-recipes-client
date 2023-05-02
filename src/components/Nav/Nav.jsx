95e598b3523ed9ce7abdafa065fac9be8f50fc83
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-02 20:41:19 +0600

    added navigation bar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Nav.css'
+import { Link } from 'react-router-dom';
+
+
+
+const Nav = () => {
+    return (
+        <nav className='Nav'>
+            <h2>Chef's Recipes</h2>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+                {/* {user && <span className='text-white'>Welcome {user.email} <button className='btn-out' onClick={handleLogout}>SignOut</button></span> } */}
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Layout/Home";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+  path: '/',
+  element: <Home></Home>,
+  // children: [
+  //   {
+  //     path: '/',
+  //     element: 
+  //   }
+  //   ]
+  }
+  ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
