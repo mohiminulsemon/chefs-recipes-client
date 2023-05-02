27a4071dc02f184356d5f4a9b005bc32f19818bc
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-02 20:42:02 +0600

    added banner section

---
+++ src/components/Banner/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+      <header className='header'>
+      <div className="banner-content">
+        <h1>Welcome to Chef's Recipes</h1>
+        <p>Discover delicious recipes from a professional chef</p>
+        <button>View more</button>
+      </div>
+    </header>
+    );
+};
+
+export default Header;
